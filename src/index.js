b0dfc7e430359c724e95aaf6205c48fddf526fc0
Pranavb1302 <p.bhosale.2410@gmail.com>
2025-09-09 16:31:15 +0530

    connect to MONGO atlas

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// here we need to import the .env as early as possible in all the files it is available at the start of the program 
+
+// require('dotenv').config({path:"./env"}); this messes up the consistecy of the code so it is being handled as
+import dotenv from 'dotenv'
+
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
